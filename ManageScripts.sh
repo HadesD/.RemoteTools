4b5ecd111c8e110180d0b0322a0df11aeaef2018
Ichiro <dark.hades.1102@gmail.com>
2021-08-19 04:28:57 +0900

    Update

--- ManageScripts.sh
+++ ManageScripts.sh
@@ -6,15 +6,45 @@ read -p '[+] Choose Action (Exit=CTRL+C):
 - [2] Update (with git pull)
 - [3] git push
 - [4] Install
-- [5] Init
-Default[0]: ' ACTION_OPT
+Default[*]: ' ACTION_OPT
 
 case $ACTION_OPT in
+  1)
+    echo '!!! Please keep this window open. Only close the WinSCP window !!!'
+    cp WinSCP/WinSCP.ini .RemoteTools/WinSCP.ini
+    .RemoteTools/WinSCP/WinSCP.exe
+    cp .RemoteTools/WinSCP.ini WinSCP/WinSCP.ini
+    ;;
+
+  2)
+    git pull
+    git submodule update --init --recursive
+    git submodule foreach git checkout master
+    git submodule foreach git pull
+    ;;
+
+  3)
+    git add .
+    git commit -m "Update"
+    git push
+    ;;
+
+  4)
+    git submodule update --init --recursive
+    git submodule foreach git pull
+    git submodule foreach git checkout master
+    git submodule foreach git pull
+
+    curl -L https://github.com/HadesD/.RemoteTools/releases/download/latest/RemoteTools.tar.gz --output RemoteTools.tar.gz
+    tar -zxvf RemoteTools.tar.gz -C ./.RemoteTools/
+    rm -f RemoteTools.tar.gz
+    ;;
+
   *)
     cd SSH
-    ls -la
-    read -p 'Enter FileName: ' SSH_TARGET_FILE_NAME
-
+    ls -Al *.sh
+    read -p '[+] Enter FileName: ' SSH_TARGET_FILE_NAME
+    bash $SSH_TARGET_FILE_NAME
     ;;
 esac
 
