01404d3a596e10c6ef8559444a7417b348d5a5e5
Ichiro <dark.hades.1102@gmail.com>
2021-08-21 16:04:48 +0900

    Update

--- ManageScripts.sh
+++ ManageScripts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-read -p '[+] Choose Action (Exit=CTRL+C):
+main() {
+  read -p '[+] Choose Action (Exit=CTRL+C):
 - [*] SSH Remote
 - [1] WinSCP Stand-Alone
 - [2] Navicat Stand-Alone
@@ -10,65 +11,79 @@ read -p '[+] Choose Action (Exit=CTRL+C):
 - [6] Create new SSH
 Default[*]: ' ACTION_OPT
 
-case $ACTION_OPT in
-  1)
-    echo '!!! Please keep this window open. Only close the WinSCP window !!!'
-    cp WinSCP/WinSCP.ini .RemoteTools/WinSCP/WinSCP.ini
-    .RemoteTools/WinSCP/WinSCP.exe
-    cp .RemoteTools/WinSCP.ini WinSCP/WinSCP.ini
-    ;;
+  case $ACTION_OPT in
+    1)
+      echo '!!! Please keep this window open. Only close the WinSCP window !!!'
+      cp WinSCP/WinSCP.ini .RemoteTools/WinSCP/WinSCP.ini
+      .RemoteTools/WinSCP/WinSCP.exe
+      cp .RemoteTools/WinSCP.ini WinSCP/WinSCP.ini
+      ;;
 
-  2)
-    cd SSH
-    start ../.RemoteTools/Navicat/navicat.exe
-    ;;
+    2)
+      cd SSH
+      start ../.RemoteTools/Navicat/navicat.exe
+      cd ..
+      ;;
 
-  3)
-	git submodule foreach git checkout .
-	git submodule foreach git checkout master
-	git submodule foreach git pull
-	git pull
-    ;;
+    3)
+      git submodule foreach git checkout .
+      git submodule foreach git checkout master
+      git submodule foreach git pull
+      git pull
+      ;;
 
-  4)
-    git add .
-    git commit -m "Update"
-    git push
-    ;;
+    4)
+      git add .
+      git commit -m "Update"
+      git push
+      ;;
 
-  5)
-    git submodule update --init --recursive
-    git submodule foreach git checkout .
-    git submodule foreach git pull
-    git submodule foreach git checkout master
-    git submodule foreach git pull
+    5)
+      git submodule update --init --recursive
+      git submodule foreach git checkout .
+      git submodule foreach git pull
+      git submodule foreach git checkout master
+      git submodule foreach git pull
 
-    curl -L https://github.com/HadesD/.RemoteTools/releases/download/latest/RemoteTools.tar.gz --output RemoteTools.tar.gz
-    tar -zxvf RemoteTools.tar.gz -C ./.RemoteTools/
-    rm -f RemoteTools.tar.gz
-    ;;
+      curl -L https://github.com/HadesD/.RemoteTools/releases/download/latest/RemoteTools.tar.gz --output RemoteTools.tar.gz
+      tar -zxvf RemoteTools.tar.gz -C ./.RemoteTools/
+      rm -f RemoteTools.tar.gz
+      ;;
 
-  6)
-    read -p '[+] Enter SSH_SERVER_NAME: ' SSH_SERVER_NAME
-    read -p '[+] Enter REMOTE_SERVER_HOST: ' REMOTE_SERVER_HOST
-    read -p '[+] Enter REMOTE_SERVER_PORT: ' REMOTE_SERVER_PORT
-    read -p '[+] Enter REMOTE_SERVER_USERNAME: ' REMOTE_SERVER_USERNAME
-    read -p '[+] Enter REMOTE_SERVER_PASSWORD: ' REMOTE_SERVER_PASSWORD
-    vim Keys/${SSH_SERVER_NAME}.pem
-    echo "#!/bin/bash
+    6)
+      read -p '[+] Enter SSH_SERVER_NAME: ' SSH_SERVER_NAME
+      read -p '[+] Enter REMOTE_SERVER_HOST: ' REMOTE_SERVER_HOST
+      read -p '[+] Enter REMOTE_SERVER_PORT: ' REMOTE_SERVER_PORT
+      read -p '[+] Enter REMOTE_SERVER_USERNAME: ' REMOTE_SERVER_USERNAME
+      read -p '[+] Enter REMOTE_SERVER_PASSWORD: ' REMOTE_SERVER_PASSWORD
+      vim Keys/${SSH_SERVER_NAME}.pem
+      cat > SSH/${SSH_SERVER_NAME}.sh <<'_EOF'
+#!/bin/bash
 
 REMOTE_SERVER_HOST=${REMOTE_SERVER_HOST}
 REMOTE_SERVER_PORT=${REMOTE_SERVER_PORT}
 REMOTE_SERVER_USERNAME=${REMOTE_SERVER_USERNAME}
 REMOTE_SERVER_PASSWORD='${REMOTE_SERVER_PASSWORD}'
 
-source ../.RemoteTools/SSH/exec-remote.sh" >> SSH/${SSH_SERVER_NAME}.sh
-    ;;
+source ../.RemoteTools/SSH/exec-remote.sh
+_EOF
+      ;;
 
-  *)
-    cd SSH
-    ls -Al *.sh
-    read -p '[+] Enter FileName: ' SSH_TARGET_FILE_NAME
-    bash $SSH_TARGET_FILE_NAME
-    ;;
-esac
+    *)
+      cd SSH
+      ls -Al *.sh
+      read -p '[+] Enter FileName: ' SSH_TARGET_FILE_NAME
+      if [[ "${SSH_TARGET_FILE_NAME}" != "" ]]; then
+        bash $SSH_TARGET_FILE_NAME
+      else
+        echo '(exit)'
+      fi
+      cd ..
+      ;;
+  esac
+
+  main
+}
+
+main
+
